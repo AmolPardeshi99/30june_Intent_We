b6b6f97789c5940ec1d930a679c6409360b3992a
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-06-30 18:27:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a30june_intent_we/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.a30june_intent_we;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView tvShowUserName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tvShowUserName = findViewById(R.id.tvShowUserName);
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+        tvShowUserName.setText(userName);
+    }
+}
